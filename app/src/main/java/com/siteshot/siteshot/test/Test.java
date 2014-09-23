8f098907eb1e3e4c65d88d22d14ebb779f7b16a8
Andrew Clissold <andrewclissold@gmail.com>
2014-09-23 19:01:46 -0400

    Test parse integration

--- app/src/main/java/com/siteshot/siteshot/test/Test.java
+++ app/src/main/java/com/siteshot/siteshot/test/Test.java
@@ -1,14 +1,32 @@
 package com.siteshot.siteshot.test;
 
-import android.test.InstrumentationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.parse.ParseUser;
+import com.siteshot.siteshot.TabActivity;
 
 /**
  * Created by aclissold on 9/23/14.
  */
-public class Test extends InstrumentationTestCase {
+public class Test extends ActivityInstrumentationTestCase2<TabActivity> {
+
+    public Test() {
+        super(TabActivity.class);
+    }
+
+    @Override
+    public void setUp() {
+        // Create the activity, to call Parse.initialize();
+        getActivity();
+    }
 
     public void testTestCasesAreFunctional() throws Exception {
         assertTrue(true);
     }
 
+    public void testParseIntegration() throws Exception {
+        ParseUser.logOut();
+        ParseUser.logIn("test", "test");
+        assertNotNull(ParseUser.getCurrentUser());
+    }
 }
