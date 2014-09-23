428c4ef61b33aa946c693f34da2a9c23dce3b120
Andrew Clissold <andrewclissold@gmail.com>
2014-09-23 18:37:14 -0400

    Add first test case

---
+++ app/src/main/java/com/siteshot/siteshot/test/Test.java
@@ -0,0 +1,14 @@
+package com.siteshot.siteshot.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by aclissold on 9/23/14.
+ */
+public class Test extends InstrumentationTestCase {
+
+    public void testTestCasesAreFunctional() throws Exception {
+        assertTrue(true);
+    }
+
+}
