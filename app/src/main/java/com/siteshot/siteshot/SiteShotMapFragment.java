f3c27a3f955bb5c86cc1a26e3c0e5b1d45e709e9
Jon Wong <jwwong@oakland.edu>
2014-10-14 18:57:18 -0400

    start map marking

--- app/src/main/java/com/siteshot/siteshot/SiteShotMapFragment.java
+++ app/src/main/java/com/siteshot/siteshot/SiteShotMapFragment.java
@@ -1,5 +1,7 @@
 package com.siteshot.siteshot;
 
+import android.app.Application;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.IntentSender;
 import android.graphics.Color;
@@ -20,17 +22,29 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.ParseGeoPoint;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
 import com.siteshot.siteshot.activities.TabActivity;
 
+//import java.text.ParseException;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A placeholder fragment containing the map view.
@@ -102,6 +116,9 @@ public class SiteShotMapFragment extends Fragment implements LocationListener,
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
 
+    // Adapter for the Parse query
+    private ParseQueryAdapter<SiteShotMapData> postsQueryAdapter;
+
     /**
      * Returns a new instance of this fragment for the given section
      * number.
@@ -138,7 +155,7 @@ public class SiteShotMapFragment extends Fragment implements LocationListener,
         mapFragment.getMap().setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             public void onCameraChange(CameraPosition position) {
                 // When the camera changes, update the query
-                // doMapQuery();
+                 doMapQuery();
             }
         });
 
@@ -237,7 +254,7 @@ public class SiteShotMapFragment extends Fragment implements LocationListener,
         }
         // Update map radius indicator
         updateCircle(myLatLng);
-        //doMapQuery();
+        doMapQuery();
         //doListQuery();
     }
 
@@ -366,6 +383,118 @@ public class SiteShotMapFragment extends Fragment implements LocationListener,
         builder.include(south);
 
         return builder.build();
+    }
+
+    /*
+ * Set up the query to update the map view
+ */
+    private void doMapQuery() {
+        final int myUpdateNumber = ++mostRecentMapUpdate;
+        Location myLoc = (currentLocation == null) ? lastLocation : currentLocation;
+        // If location info isn't available, clean up any existing markers
+        if (myLoc == null) {
+            cleanUpMarkers(new HashSet<String>());
+            return;
+        }
+        final ParseGeoPoint myPoint = geoPointFromLocation(myLoc);
+        // Create the map Parse query
+        /*ParseQuery<SiteShotMapData> mapQuery = SiteShotMapData.getQuery();
+        // Set up additional query filters
+        mapQuery.whereWithinKilometers("location", myPoint, MAX_POST_SEARCH_DISTANCE);
+        mapQuery.include("user");
+        mapQuery.orderByDescending("createdAt");
+        mapQuery.setLimit(MAX_POST_SEARCH_RESULTS);
+        // Kick off the query in the background
+        mapQuery.findInBackground(new FindCallback<SiteShotMapData>() {
+            @Override
+            public void done(List<SiteShotMapData> objects, ParseException e) {
+                if (e != null) {
+                    //if (Application.APPDEBUG) {
+                    //    Log.d(Application.APPTAG, "An error occurred while querying for map posts.", e);
+                    //}
+                    return;
+                }
+                */
+        /*
+         * Make sure we're processing results from
+         * the most recent update, in case there
+         * may be more than one in progress.
+         */
+        return;
+               /* if (myUpdateNumber != mostRecentMapUpdate) {
+                    return;
+                }
+                // Posts to show on the map
+                Set<String> toKeep = new HashSet<String>();
+                // Loop through the results of the search
+                for (SiteShotMapData post : objects) {
+                    // Add this post to the list of map pins to keep
+                    toKeep.add(post.getObjectId());
+                    // Check for an existing marker for this post
+                    Marker oldMarker = mapMarkers.get(post.getObjectId());
+                    // Set up the map marker's location
+                    MarkerOptions markerOpts =
+                            new MarkerOptions().position(new LatLng(post.getLocation().getLatitude(), post
+                                    .getLocation().getLongitude()));
+                    // Set up the marker properties based on if it is within the search radius
+                    if (post.getLocation().distanceInKilometersTo(myPoint) > radius * METERS_PER_FEET
+                            / METERS_PER_KILOMETER) {
+                        // Check for an existing out of range marker
+                        if (oldMarker != null) {
+                            if (oldMarker.getSnippet() == null) {
+                                // Out of range marker already exists, skip adding it
+                                continue;
+                            } else {
+                                // Marker now out of range, needs to be refreshed
+                                oldMarker.remove();
+                            }
+                        }
+                        // Display a red marker with a predefined title and no snippet
+                        markerOpts =
+                                markerOpts.title(getResources().getString(R.string.post_out_of_range)).icon(
+                                        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                    } else {
+                        // Check for an existing in range marker
+                        if (oldMarker != null) {
+                            if (oldMarker.getSnippet() != null) {
+                                // In range marker already exists, skip adding it
+                                continue;
+                            } else {
+                                // Marker now in range, needs to be refreshed
+                                oldMarker.remove();
+                            }
+                        }
+                        // Display a green marker with the post information
+                        markerOpts =
+                                markerOpts.title(post.getText()).snippet(post.getUser().getUsername())
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                    }
+                    // Add a new marker
+                    Marker marker = mapFragment.getMap().addMarker(markerOpts);
+                    mapMarkers.put(post.getObjectId(), marker);
+                    if (post.getObjectId().equals(selectedPostObjectId)) {
+                        marker.showInfoWindow();
+                        selectedPostObjectId = null;
+                    }
+                }
+                // Clean up old markers.
+                cleanUpMarkers(toKeep);
+            }
+        });*/
+    }
+
+    /*
+     * Helper method to clean up old markers
+     */
+    private void cleanUpMarkers(Set<String> markersToKeep) {
+        for (String objId : new HashSet<String>(mapMarkers.keySet())) {
+            if (!markersToKeep.contains(objId)) {
+                Marker marker = mapMarkers.get(objId);
+                marker.remove();
+                mapMarkers.get(objId).remove();
+                mapMarkers.remove(objId);
+            }
+        }
     }
 
 
