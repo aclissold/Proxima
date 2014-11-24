9334e7353b345cd67a3eaf746626e7f5428326e2
Jon Wong <jwwong@oakland.edu>
2014-11-24 18:56:42 -0500

    Update the badge icons on cluster view, add exit button to cluster view

--- app/src/main/java/com/proxima/activities/ClusterViewActivity.java
+++ app/src/main/java/com/proxima/activities/ClusterViewActivity.java
@@ -2,7 +2,9 @@ package com.proxima.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageButton;
 
 import com.proxima.R;
 import com.proxima.adapters.ClusterImageAdapter;
@@ -22,6 +24,8 @@ public class ClusterViewActivity extends ActionBarActivity {
     ArrayList<String> clusterContents;
     ArrayList<String> newlyDiscovered;
 
+    ImageButton mExitButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -33,8 +37,16 @@ public class ClusterViewActivity extends ActionBarActivity {
         newlyDiscovered = new ArrayList<String>(Arrays.asList(discoveredArr));
         count = clusterContents.size();
         setContentView(R.layout.activity_cluster_view);
+        mExitButton = (ImageButton) findViewById(R.id.exitButton);
         GridView photoGrid = (GridView) findViewById(R.id.cluster_photo_grid);
         photoGrid.setAdapter(new ClusterImageAdapter(this, this));
+
+        mExitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
 
