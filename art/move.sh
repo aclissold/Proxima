c909ac39af7a9afb8b026949669e89cb416f2d54
Andrew Clissold <andrewclissold@gmail.com>
2014-11-24 16:48:36 -0500

    Create new shutter button icon

---
+++ art/move.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv shutter-mdpi.png ../app/src/main/res/drawable-mdpi/shutter.png
+mv shutter-hdpi.png ../app/src/main/res/drawable-hdpi/shutter.png
+mv shutter-xhdpi.png ../app/src/main/res/drawable-xhdpi/shutter.png
+mv shutter-xxhdpi.png ../app/src/main/res/drawable-xxhdpi/shutter.png
